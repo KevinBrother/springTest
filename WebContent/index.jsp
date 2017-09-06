22a23b911f72d650693ba6f07976d3d2ec4acca3
KevinBrother <1301239018@qq.com>
2017-09-06 17:05:42 +0800

    环境搭建，获取请求参数和视图解析器 modeandView

---
+++ WebContent/index.jsp
@@ -0,0 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+<a href="helloworld">hello world</a><br/><br/>
+
+<a href="springmvc/testPathVariable/12">testPathVariable</a><br/><br/>
+
+<a href="springmvc/testRequestParam?username=joke&age=20">testRequestParam</a><br/><br/>
+
+<a href="springmvc/testCookieValue">testCookieValue</a><br/><br/>
+
+<a href="springmvc/testRequestHeader">testRequestHeader</a><br/><br/>
+
+<form action="springmvc/testPojo" method="post">
+    username: <input type="text" name="username"><br>
+    password: <input type="password" name="password"><br>
+    email: <input type="text" name="email"><br>
+    age: <input type="text" name="age"><br>
+    city: <input type="text" name="address.city"><br>
+    province: <input type="text" name="address.province"><br>
+    <input type="submit" value="submit">
+</form><br/><br/>
+
+<a href="springmvc/testModelAndView">testModelAndView</a><br/><br/>	
+
+
+</body>
+</html>
---
+++ src/com/jackie/springmvc/handlers/SpringMVCTest.java
@@ -0,0 +1,64 @@
+package com.jackie.springmvc.handlers;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.my.domain.User;
+
+
+/**
+ * 请求参数实例
+ * @author heheda
+ *
+ */
+@RequestMapping("/springmvc")
+@Controller
+public class SpringMVCTest {
+	
+	@RequestMapping("/testPathVariable/{id}")
+	//http://localhost:8091/springTest/springmvc/testPathVariable/12
+	public String testPathVariable(@PathVariable(value="id") Integer id) {
+		System.out.println("testPathVariable=" + id);
+		return "/success";
+	}
+	
+	@RequestMapping("/testRequestParam")
+	//http://localhost:8091/springTest/springmvc/testPathVariable/12
+	public String testRequestParam(@RequestParam(value="username") String username, @RequestParam(value="age", required=false, defaultValue="0") int age) {
+		System.out.println("username=" + username + ",age=" + age);
+		return "/success";
+	}
+	
+	@RequestMapping("/testCookieValue")
+	//http://localhost:8091/springTest/springmvc/testPathVariable/12
+	public String testCookieValue(@CookieValue("JSESSIONID") String JSESSIONID) {
+		System.out.println("JSESSIONID=" + JSESSIONID);
+		return "/success";
+	}
+	
+	@RequestMapping("/testRequestHeader")
+	//http://localhost:8091/springTest/springmvc/testPathVariable/12
+	public String testRequestHeader(@RequestHeader(value="Accept-Language") String language) {
+		System.out.println("testRequestHeader Accept-Languge:" + language);
+		return "/success";
+	}
+	
+	@RequestMapping("/testPojo")
+	//http://localhost:8091/springTest/springmvc/testPathVariable/12
+	public String testPojo(User user) {
+		System.out.println("testPojo" + user);
+		return "/success";
+	}
+	
+	@RequestMapping("/testModelAndView")
+	//http://localhost:8091/springTest/springmvc/testPathVariable/12
+	public ModelAndView testModelAndView() {
+		String viewname= "/success";
+		ModelAndView modelAndView = new ModelAndView(viewname);
+		modelAndView.addObject("time", new Date());
+		return modelAndView;
+	}
+}
